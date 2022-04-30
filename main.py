de66695add4ed6f415d1f497a3f7fc73dc9c16e7
206 <206@academicos.cesde.edu.co>
2022-04-29 20:30:13 -0500

    Herencia

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #Herencia en python
+
+from clases.Cliente import Cliente
+
+#crear un OBJETO de la clase Cliente
+hijo=Cliente()
+
+hijo.nombre="Juan Carlos Ardila"
+hijo.saldo=5000
+
+hijo.clasificar()
